fa08632788af9ac63642206e5fb5884d44506293
RakhaMA <rakalani8@gmail.com>
2023-03-20 23:21:36 +0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('ISTIANJING');
+	res.send('123190109\nRakha Mohammad A');
 });
 
 const PORT = process.env.PORT || 8080;
